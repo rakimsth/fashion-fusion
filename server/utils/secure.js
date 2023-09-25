9be39ba9963819233e67192a4c7bf29ab44d438c
Raktim Shrestha <raktim@rumsan.com>
2023-09-25 17:45:06 +0545

    secure update for roles

--- server/utils/secure.js
+++ server/utils/secure.js
@@ -1,4 +1,5 @@
 const { verifyJWT } = require("./jwt");
+const userModel = require("../modules/users/user.model");
 
 const compareRole = (requiredRole, userRole) => {
   if (requiredRole.length < 1) return true;
@@ -6,17 +7,25 @@ const compareRole = (requiredRole, userRole) => {
 };
 
 const secureAPI = (roles) => {
-  return (req, res, next) => {
-    const token = req?.headers?.authorization;
-    if (!token) throw new Error("Access Token Required");
-    const accessToken = token.split("Bearer ")[1];
-    const { data } = verifyJWT(accessToken);
-    const { roles: userRole, email } = data;
-    // check if the user has required role or not
-    // compare role against secureAPI passed role
-    const isValidRole = compareRole(roles ?? [], userRole);
-    if (!isValidRole) throw new Error("User unauthorized");
-    next();
+  return async (req, res, next) => {
+    try {
+      const token = req?.headers?.authorization;
+      if (!token) throw new Error("Access Token Required");
+      const accessToken = token.split("Bearer ")[1];
+      const { data } = verifyJWT(accessToken);
+      const { email } = data;
+      // check if the user has required role or not
+      const user = await userModel.findOne({ email });
+      if (!user) throw new Error("User not found");
+      req.currentUser = user?._id;
+      req.currentRoles = user?.roles;
+      // compare role against secureAPI passed role
+      const isValidRole = compareRole(roles ?? [], user?.roles);
+      if (!isValidRole) throw new Error("User unauthorized");
+      next();
+    } catch (e) {
+      next(e);
+    }
   };
 };
 
