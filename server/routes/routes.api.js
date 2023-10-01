428a1e52fe52825f575c681e68fea1c1cfd081ee
Raktim Shrestha <raktim@rumsan.com>
2023-10-01 16:52:02 +0545

    api endpoint connection

--- server/routes/routes.api.js
+++ server/routes/routes.api.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 const authRouter = require("../modules/auth/auth.routes");
 const categoryRouter = require("../modules/categories/category.routes");
+const orderRouter = require("../modules/orders/order.routes");
+const productRouter = require("../modules/products/product.routes");
 const userRouter = require("../modules/users/user.routes");
 
 router.get("/", (req, res, next) => {
@@ -10,6 +12,8 @@ router.get("/", (req, res, next) => {
 
 router.use("/auth", authRouter);
 router.use("/category", categoryRouter);
+router.use("/orders", orderRouter);
+router.use("/products", productRouter);
 router.use("/users", userRouter);
 
 router.all("*", (req, res, next) => {
