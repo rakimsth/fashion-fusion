8f5da7e21db9e5878e3199fd97ab300010f2cf08
Raktim Shrestha <raktim@rumsan.com>
2023-10-09 17:46:17 +0545

    cleanup

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -65,100 +65,98 @@ const FilledCart = ({
 }) => {
   return (
     <>
-      <>
-        <h1 className="text-center m-4">Your Cart</h1>
-        <div className="row">
-          <div className="col-md-12">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Image</th>
-                  <th>Price (NPR)</th>
-                  <th>Quantity</th>
-                  <th>Total Price (NPR)</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {items.map((item, index) => {
-                  return (
-                    <tr key={item?.id || index}>
-                      <td>
-                        {item?.title.length > 25
-                          ? item?.title.substring(0, 60).concat("...")
-                          : item?.title}
-                      </td>
-                      <td>
-                        <Image
-                          width={40}
-                          height={40}
-                          src={item?.image}
-                          thumbnail
-                        />
-                      </td>
-                      <td>
-                        {numberFormatter("#,##,###.##", Number(item?.price))}
-                      </td>
-                      <td>
-                        <span
-                          className="btn btn-primary"
-                          style={{ margin: "2px" }}
-                          onClick={() => {
-                            decrease(item?.id);
-                          }}
-                        >
-                          -
-                        </span>
-                        <span className="btn btn-info">{item?.quantity}</span>
-                        <span
-                          className="btn btn-primary"
-                          style={{ margin: "2px" }}
-                          onClick={() => {
-                            increase(item?.id);
-                          }}
-                        >
-                          +
-                        </span>
-                      </td>
-                      <td>
-                        {numberFormatter(
-                          "#,##,###.##",
-                          Number(item?.price) * Number(item?.quantity)
-                        )}
-                      </td>
+      <h1 className="text-center m-4">Your Cart</h1>
+      <div className="row">
+        <div className="col-md-12">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Image</th>
+                <th>Price (NPR)</th>
+                <th>Quantity</th>
+                <th>Total Price (NPR)</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item, index) => {
+                return (
+                  <tr key={item?.id || index}>
+                    <td>
+                      {item?.title.length > 25
+                        ? item?.title.substring(0, 60).concat("...")
+                        : item?.title}
+                    </td>
+                    <td>
+                      <Image
+                        width={40}
+                        height={40}
+                        src={item?.image}
+                        thumbnail
+                      />
+                    </td>
+                    <td>
+                      {numberFormatter("#,##,###.##", Number(item?.price))}
+                    </td>
+                    <td>
+                      <span
+                        className="btn btn-primary"
+                        style={{ margin: "2px" }}
+                        onClick={() => {
+                          decrease(item?.id);
+                        }}
+                      >
+                        -
+                      </span>
+                      <span className="btn btn-info">{item?.quantity}</span>
+                      <span
+                        className="btn btn-primary"
+                        style={{ margin: "2px" }}
+                        onClick={() => {
+                          increase(item?.id);
+                        }}
+                      >
+                        +
+                      </span>
+                    </td>
+                    <td>
+                      {numberFormatter(
+                        "#,##,###.##",
+                        Number(item?.price) * Number(item?.quantity)
+                      )}
+                    </td>
 
-                      <td>
-                        <AiFillCloseCircle
-                          color="red"
-                          size={24}
-                          onClick={() => {
-                            removeFromCart(item?.id);
-                          }}
-                        />
-                      </td>
-                    </tr>
-                  );
-                })}
-                <tr>
-                  <td colSpan="5">Total Carts</td>
-                  <td>
-                    {numberFormatter("NPR #,##,###.##", Number(getTotal()))}
-                  </td>
-                </tr>
-                <tr>
-                  <td colSpan="5">
-                    <Link to="/products">Continue Shopping</Link>
-                  </td>
-                  <td>
-                    <Link to="/checkout">Check Out Now</Link>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+                    <td>
+                      <AiFillCloseCircle
+                        color="red"
+                        size={24}
+                        onClick={() => {
+                          removeFromCart(item?.id);
+                        }}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+              <tr>
+                <td colSpan="5">Total Carts</td>
+                <td>
+                  {numberFormatter("NPR #,##,###.##", Number(getTotal()))}
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="5">
+                  <Link to="/products">Continue Shopping</Link>
+                </td>
+                <td>
+                  <Link to="/checkout">Check Out Now</Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      </>
+      </div>
     </>
   );
 };
